6ab783cd20c4f6d95a0276ce9d1d2cd579560f6b
Jefferson Araújo <jeffersonc201212@gmail.com>
2022-11-06 12:55:26 -0300

    Listagem dos quizzes do usuário implementada.

--- js/listaDeQuizzes.js
+++ js/listaDeQuizzes.js
@@ -4,9 +4,9 @@ const listaDeQuizzes = document.createElement("div");
 listaDeQuizzes.classList.add("lista-de-quizzes");
 homePage();
 
-function homePage(){
+function homePage() {
   app.innerHTML = "";
-  
+
   //Solicita ao servidor as informações dos quizzes
   const promiseQuizzes = axios.get(
     "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
@@ -22,21 +22,52 @@ function homePage(){
 
 //Funcao que renderiza a seçao 'meus quizzes'
 function viewMeusQuizzes() {
-  const meusQuizzesSection = document.createElement("section");
-  meusQuizzesSection.classList.add("meus-quizzes");
+  let quizzes = localStorage.getItem("meus-quizzes");
 
-  meusQuizzesSection.innerHTML = /*HTML*/ `
-    <p>Você não criou nenhum quizz ainda :(</p>
-  `;
-  const btnAddQuiz = document.createElement("button");
-  btnAddQuiz.classList.add("meus-quizzes__btn-add-quiz");
-  btnAddQuiz.innerHTML = "Criar Quizz";
+  if (quizzes === null) {
+    const meusQuizzesSection = document.createElement("section");
+    meusQuizzesSection.classList.add("meus-quizzes");
 
-  btnAddQuiz.onclick = () => btnAddQuizClickController();
+    meusQuizzesSection.innerHTML = /*HTML*/ `
+      <p>Você não criou nenhum quizz ainda :(</p>
+    `;
+    const btnAddQuiz = document.createElement("button");
+    btnAddQuiz.classList.add("meus-quizzes__btn-add-quiz");
+    btnAddQuiz.innerHTML = "Criar Quizz";
 
-  meusQuizzesSection.appendChild(btnAddQuiz);
-  listaDeQuizzes.appendChild(meusQuizzesSection);
-  app.appendChild(listaDeQuizzes);
+    btnAddQuiz.onclick = () => btnAddQuizClickController();
+
+    meusQuizzesSection.appendChild(btnAddQuiz);
+    listaDeQuizzes.appendChild(meusQuizzesSection);
+    app.appendChild(listaDeQuizzes);
+  } else {
+    quizzes = JSON.parse(quizzes);
+    const meusQuizzes = document.createElement("section"); //captura a area dos quizzes
+    meusQuizzes.classList.add("meus-quizzes--preenchidos"); //adiciona a classe
+    meusQuizzes.innerHTML = /*HTML*/ `
+      <h2 class="titulo-secao">Meus Quizzes</h2>
+    `;
+
+    //Para cada quiz no vetor quizzes gera o render
+    quizzes.forEach((quizId) => {
+      let promise = axios.get(
+        `https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${quizId}`
+      );
+      promise.then(res => {
+        const cardQuiz = document.createElement("div");
+        cardQuiz.classList.add("card-quiz");
+        cardQuiz.innerHTML = /*HTML*/ `
+          <h3 class="card-quiz__title">${res.data.title}</h3>
+        `;
+        cardQuiz.style.backgroundImage = `url(${res.data.image})`;
+
+        cardQuiz.onclick = () => clickCardQuizController();
+        meusQuizzes.appendChild(cardQuiz);
+      }) 
+    });
+    listaDeQuizzes.appendChild(meusQuizzes);
+    app.appendChild(listaDeQuizzes);
+  }
 }
 
 //Função que renderiza os cards dos quizzes e seçao 'todos os quizzes'
